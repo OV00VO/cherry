57b2abef5af77e396ea922c17fcf900b94f36770
OV00VO V <oskar.d.vikstrom@gmail.com>
2024-06-22 11:56:23 +0000

    Test to deploy without numpy in requirements, run error.

--- src/machine_learning/predictive_analysis.py
+++ src/machine_learning/predictive_analysis.py
@@ -49,7 +49,7 @@ def load_model_and_predict(my_image, version):
     Load and perform ML prediction over live images
     """
 
-    model = load_model(f"outputs/{version}/mold_detector_model.h5")
+    model = load_model(f"outputs/{version}/mmildew_detector_model.h5")
 
     pred_proba = model.predict(my_image)[0, 0]
 
@@ -59,7 +59,7 @@ def load_model_and_predict(my_image, version):
         pred_proba = 1 - pred_proba
 
     st.write(
-        f"The predictive analysis indicates the sample leave is "
-        f"**{pred_class.lower()}** with mold.")
+        f"The predictive analysis indicates the sample leaf is "
+        f"**{pred_class.lower()}** with mildew.")
 
     return pred_proba, pred_class
