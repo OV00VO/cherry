20846764828e5b1534d51091a96403e74bf6bc4b
OV00VO V <oskar.d.vikstrom@gmail.com>
2024-06-22 14:32:20 +0000

    Fix requirements.txt, Heroku too large needed to be below 500 MB.

--- app_pages/page_summary.py
+++ app_pages/page_summary.py
@@ -7,20 +7,16 @@ def page_summary_body():
     st.write("### Quick Project Summary")
 
     st.info(
-        f"**General Information**\n"
-        f"x"
-        f"x\n"
-        f"x"
-        f"Visual criteria are used to detect mildew on cherry leaves.\n"
+        f"**General Information**\n\n"
+        f"This visual criteria are used to detect mildew on cherry leaves:\n\n"
         f"Cherry leaf spot shows as purple spots turning brown or gray, "
-        f"with possible white fungus in the center on top of leaves. "
+        f"with possible white fungus in the center on top of leaves. \n\n"
         f"Powdery mildew shows as white patches on the underside of leaves, "
-        f"which may become distorted."
+        f"which may become distorted.\n"
         f"Look for these signs on young leaves and in the inner canopy.\n\n"
         f"**Project Dataset**\n"
-        f"* The available dataset contains x out of +x thousand images taken from "
-        f"x"
-        f"x")
+        f"* The available dataset contains of in total 4208 in a\n 50 percent split of infecten and uninfected leaves."
+        )
 
     st.write(
         f"* For additional information, please visit and **read** the "
@@ -30,6 +26,6 @@ def page_summary_body():
     st.success(
         f"The project has 2 business requirements:\n"
         f"* 1 - The client is interested in having a study to differentiate "
-        f"infected with mildew and uninfected with mold in a visually way.\n"
+        f"infected with mildew and uninfected with mildew, in a visually way.\n"
         f"* 2 - The client is interested in telling whether a given leaf contains mildew on the cherry leaf or not. "
         )
