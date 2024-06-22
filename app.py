d165af03db4bb2d7542e097a90acbac4e59b2e76
OV00VO V <oskar.d.vikstrom@gmail.com>
2024-06-22 14:08:39 +0000

    Fix versions in requirements.txt, Heroku too large needed to be below 500 MB.

--- app.py
+++ app.py
@@ -8,12 +8,12 @@ from app_pages.page_mildew_detector import page_mildew_detector_body
 from app_pages.page_project_hypothesis import page_project_hypothesis_body
 from app_pages.page_ml_performance import page_ml_performance_metrics
 
-app = MultiPage(app_name="Cherry Leave Mold Detector")  # Create an instance of the app
+app = MultiPage(app_name="Cherry Leaf Mildew Detector")  # Create an instance of the app
 
 # Add your app pages here using .add_page()
 app.app_page("Quick Project Summary", page_summary_body)
-app.app_page("Cherry Leaves Mildew Visualiser", page_mildew_visualizer_body)
-app.app_page("Cherry Leaves Mildew Detection", page_mildew_detector_body)
+app.app_page("Cherry Leaf Mildew Visualiser", page_mildew_visualizer_body)
+app.app_page("Cherry Leaf Mildew Detection", page_mildew_detector_body)
 app.app_page("Project Hypothesis", page_project_hypothesis_body)
 app.app_page("ML Performance Metrics", page_ml_performance_metrics)
 
