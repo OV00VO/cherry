c80b08bf4118b6e5b7dab9530111c29ac8db0627
OV00VO V <oskar.d.vikstrom@gmail.com>
2024-06-23 08:40:34 +0000

    Add new plots

--- app.py
+++ app.py
@@ -1,20 +1,18 @@
 import streamlit as st
 from app_pages.multipage import MultiPage
 
-# load pages scripts
 from app_pages.page_summary import page_summary_body
 from app_pages.page_mildew_visualizer import page_mildew_visualizer_body
 from app_pages.page_mildew_detector import page_mildew_detector_body
 from app_pages.page_project_hypothesis import page_project_hypothesis_body
 from app_pages.page_ml_performance import page_ml_performance_metrics
 
-app = MultiPage(app_name="Cherry Leaf Mildew Analysis")  # Create an instance of the app
+app = MultiPage(app_name="Cherry Leaf Mildew Analysis")
 
-# Add your app pages here using .add_page()
 app.app_page("Quick Project Summary", page_summary_body)
 app.app_page("Cherry Leaf Mildew Visualiser", page_mildew_visualizer_body)
 app.app_page("Cherry Leaf Mildew Detection", page_mildew_detector_body)
 app.app_page("Project Hypothesis", page_project_hypothesis_body)
 app.app_page("ML Performance Metrics", page_ml_performance_metrics)
 
-app.run()  # Run the app
+app.run()
